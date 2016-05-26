cb1ff41490fab36e1093932eb8dd9bdfe525472c
Jonny <jonny.covert@gmail.com>
2016-05-25 21:57:14 -0400

    Added react stuff.

--- www/src/ts/app.tsx
+++ www/src/ts/app.tsx
@@ -1,3 +1,5 @@
 import { Greetings } from "./test"
 
+import * as React from 'react';
+
 document.getElementById('test').innerText = Greetings.greet("Jonny");
--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,15 +1,15 @@
 module.exports = {  
-  entry: './src/ts/app.ts',
+  entry: './src/ts/app.tsx',
   output: {
     filename: 'app/js/app.js'
   },
   resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
   },
   module: {
     loaders: [
       { 
-        test: /\.ts$/, 
+        test: /\.ts/, 
         loader: 'babel-loader?presets[]=es2015!ts-loader',
         exclude: /node_modules/
       }
