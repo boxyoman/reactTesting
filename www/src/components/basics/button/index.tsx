194a9e957383e8400a47a6c6eb4ba71d89f588ad
Jonny <jonny.covert@gmail.com>
2016-06-04 14:29:04 -0400

    Components are now function components.

--- www/src/components/basics/button/index.tsx
+++ www/src/components/basics/button/index.tsx
@@ -1,30 +1,29 @@
 import * as React from "react";
 const styles:any = require("./button.css");
 
-interface ButtonProps {
+interface IButtonProps extends React.Props<any> {
   color?: "Red" | "Green" | "Grey";
   onClick: React.EventHandler<React.MouseEvent>;
 }
 
-class RAButton extends React.Component<ButtonProps, {}> {
-  private style :any;
-  constructor(props: ButtonProps){
-    super(props);
-    this.style = styles.grey;
-    if(props.color == "Green"){
-      this.style = styles.green;
-    }else if(props.color == "Red"){
-      this.style = styles.red;
-    }
+function RAButton({
+  color,
+  onClick,
+  children
+}: IButtonProps) {
+  var style :any =styles.grey; 
+  if(color == "Green"){
+    style = styles.green;
+  }else if(color == "Red"){
+    style = styles.red;
   }
-  render() {
-    return (
-      <div className={ this.style }
-           onClick={ e => this.props.onClick(e) }>
-        {this.props.children}
-      </div>
-    );
-  }
+  return (
+    <div className={ style }
+    onClick={ e => onClick(e) }>
+    {children}
+    </div>
+  );
+
 }
 
 export default RAButton;
--- www/src/components/counter.tsx
+++ www/src/components/counter.tsx
@@ -5,22 +5,22 @@ import { IDispatch } from 'redux'
 import {increase, decrease} from '../actions/'
 import topState from '../state'
 
-interface CounterProps{
+interface ICounterProps {
   number: number,
   onIncrease: () => void,
   onDecrease: () => void
 }
 
-class Counter extends React.Component<CounterProps, {}> {
-  render() {
-    return <div>
-      <RAButton color="Green" 
-                onClick={ e => this.props.onIncrease()}>+</RAButton>
-      <RAButton color="Red" 
-                onClick={ e => this.props.onDecrease() }>-</RAButton>
-      <div>{this.props.number}</div>
-    </div>;
-  }
+function Counter({
+  number,
+  onIncrease,
+  onDecrease
+}:ICounterProps){
+  return (<div>
+    <RAButton color="Green" onClick={ e => onIncrease()}>+</RAButton>
+    <RAButton color="Red" onClick={ e => onDecrease() }>-</RAButton>
+    <div>{number}</div>
+  </div>);
 }
 
 const mapStateToProps = (state:topState) => {
